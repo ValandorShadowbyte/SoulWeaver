fe27cd030777f676e6cefd1111ec20789e40c9bb
ValandorShadowbyte <217002862+ValandorShadowbyte@users.noreply.github.com>
2025-06-19 02:35:55 -0400

    Initial commit

---
+++ Source/SoulTransfer/SoulTransferMod.cs
@@ -0,0 +1,13 @@
+﻿using HarmonyLib;
+using Verse;
+
+namespace SoulTransfer
+{
+    public class SoulTransferMod : Mod
+    {
+        public SoulTransferMod(ModContentPack content) : base(content)
+        {
+            new Harmony(nameof(SoulTransferMod)).PatchAll();
+        }
+    }
+}
