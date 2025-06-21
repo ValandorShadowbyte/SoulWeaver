5b5bd302a6ce23ada62030c2c4202872be608ce5
ValandorShadowbyte <217002862+ValandorShadowbyte@users.noreply.github.com>
2025-06-21 00:12:26 -0400

    add base for soul mark

---
+++ Source/SoulWeaver/Ability_SoulMark.cs
@@ -0,0 +1,13 @@
+﻿using RimWorld.Planet;
+using VFECore.Abilities;
+
+namespace SoulWeaver
+{
+    public class Ability_SoulMark : Ability
+    {
+        public virtual void Cast(params GlobalTargetInfo[] targets)
+        {
+            base.Cast(targets);
+        }
+    }
+}
--- Source/SoulWeaver/Properties/AssemblyInfo.cs
+++ Source/SoulWeaver/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SoulTransfer")]
+[assembly: AssemblyTitle("SoulWeaver")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SoulTransfer")]
+[assembly: AssemblyProduct("SoulWeaver")]
 [assembly: AssemblyCopyright("Copyright © 2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
