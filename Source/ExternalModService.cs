39ee53d93367b6b2b5483f36e006882a560b1395
ValandorShadowbyte <217002862+ValandorShadowbyte@users.noreply.github.com>
2025-07-18 00:06:58 -0400

    Add support for LifeisGame.WorldOfNaruto and new external references

---
+++ Source/ExternalModService.cs
@@ -0,0 +1,19 @@
+﻿using Verse;
+
+namespace SoulSerpent
+{
+    public static class ExternalModService
+    {
+        private static bool? _isNarutoModInstalled = null;
+
+        public static bool IsNarutoModInstalled()
+        {
+            if (_isNarutoModInstalled is null)
+            {
+                _isNarutoModInstalled = LoadedModManager.RunningModsListForReading.Any(mod => mod.PackageId == "LifeisGame.WorldOfNaruto");
+            }
+
+            return _isNarutoModInstalled.Value;
+        }
+    }
+}
